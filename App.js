887cef4abba85133784b7070fd909d494a6e7cc4
lucasalves0705 <lucasalves0705@gmail.com>
2021-10-26 20:39:57 -0300

    Criação e parametrização básica do app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import Stack from './Screens/Stack'
+
+
+export default props=>{
+
+  return(
+    <NavigationContainer>
+      <Stack></Stack>
+    </NavigationContainer>
+
+  )
+}
