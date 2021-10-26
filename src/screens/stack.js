c0eb6a6ba0721a6f74fafa7e4df7d3f006e66aa1
lucasalves0705 <lucasalves0705@gmail.com>
2021-10-26 20:49:30 -0300

    Criação da stack.js e página inicial Login

---
+++ src/screens/stack.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from "./Login"
+
+const Stack = createNativeStackNavigator();
+
+export default props=>{
+
+    return(
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login}></Stack.Screen>
+        </Stack.Navigator>
+    )
+}
